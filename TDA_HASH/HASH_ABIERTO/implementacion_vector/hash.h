e44a0f8b2a6e7149073eff0b471fd7bfb0948afe
Danikim01 <86693285+Danikim01@users.noreply.github.com>
2021-11-10 00:10:27 -0300

    Add files via upload

---
+++ TDA_HASH/HASH_ABIERTO/implementacion_vector/hash.h
@@ -0,0 +1,34 @@
+#ifndef HASH_H_
+#define HASH_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct par{
+	const char* clave;
+	void* valor;
+	struct par* siguiente;
+}par_t;
+
+typedef struct hash{
+	size_t cantidad;
+	size_t tamanio_tabla;
+	par_t** tabla;
+
+}hash_t;
+
+hash_t* hash_crear(size_t capacidad);
+
+void hash_destruir(hash_t* hash);
+
+bool hash_insertar(hash_t* hash,const char* clave,void* elemento);
+
+void* hash_buscar(hash_t* hash,const char* clave);
+
+bool hash_eliminar(hash_t* hash,const char* clave);
+
+void hash_imprimir(hash_t* hash,void (*funcion)(void* elemento));
+
+#endif //HASH_H_
---
+++ TDA_HASH/HASH_ABIERTO/implementacion_vector/main.c
@@ -0,0 +1,44 @@
+#include "hash.h"
+
+void imprimir_valor(void* valor){
+	int* n = valor;
+	printf("%d\n",*n);
+}
+
+int main(){
+
+	hash_t* hash = hash_crear(1);
+
+	int padrones[] = {3224,243,423,314345,1324,32231,2423,564,2,132,89};
+
+	hash_insertar(hash,"Lucas",padrones+0);
+	hash_insertar(hash,"Mauro",padrones+1);
+	hash_insertar(hash,"Eloy",padrones+2);
+	hash_insertar(hash,"Julian",padrones+3);
+	hash_insertar(hash,"Gabriel",padrones+4);
+	hash_insertar(hash,"Momo",padrones+5);
+	hash_insertar(hash,"tete",padrones+6);
+	hash_insertar(hash,"Koko",padrones+7);
+	hash_insertar(hash,"Huhu",padrones+8);
+	hash_insertar(hash,"ryuba",padrones+9);
+
+	printf("El padron de Mauro es: %d\n",*(int*)hash_buscar(hash,"Mauro"));
+	printf("La cantidad de elementos del hash es %d\n",hash->cantidad);
+
+	hash_imprimir(hash,imprimir_valor);
+	hash_eliminar(hash,"Mauro");
+
+	if(!hash_buscar(hash,"Mauro")){
+		printf("Al eliminar a Mauro y buscarlo devuelve NULL\n");
+	}
+
+	hash_imprimir(hash,imprimir_valor);
+
+	printf("Pruebo borrar el primero elemento a ver que onda\n");
+	hash_eliminar(hash,"Lucas");
+
+	hash_imprimir(hash,imprimir_valor);
+
+	hash_destruir(hash);
+	return 0;
+}
